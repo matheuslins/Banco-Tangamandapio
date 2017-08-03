1a721b8956e3759dc38f1c1ecff4a46f4bd3ab07
Matheus Lins <msl@cin.ufpe.br>
2017-08-03 01:44:45 -0300

    Changed cells

--- conta/models.py
+++ conta/models.py
@@ -21,22 +21,11 @@ class Conta(models.Model):
 
 
 class Caixa(models.Model):
-    CELUDAS = (
-        ('1', 'P$1'),
-        ('2', 'P$2'),
-        ('5', 'P$5'),
-        ('10', 'P$10'),
-        ('20', 'P$20'),
-        ('50', 'P$50'),
-        ('100', 'P$100'),
-    )
-    cedula = models.CharField(
-        max_length=3, choices=CELUDAS, default='1',
-        verbose_name='cedula', primary_key=True)
+    cedula = models.IntegerField(primary_key=True)
     quantidade = models.IntegerField()
 
     def __str__(self):
-        return 'P$' + self.cedula + ' -> ' + str(self.quantidade)
+        return 'P$' + str(self.cedula) + ' -> ' + str(self.quantidade)
 
     class Meta:
         verbose_name = 'Caixa'
